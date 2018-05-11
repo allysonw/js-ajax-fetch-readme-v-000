64949a524b38f8f834925e03fe4630489afb2c09
Allyson Wesman <allysonwesman@gmail.com>
2018-05-11 16:56:49 -0700

    remove index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const token = '17b2ac237aea2d5c54f760def2853bebc99c7d5b'
-
-fetch('https://api.github.com/user/repos', {
-  headers: {
-    Authorization: `token ${token}`
-  }
-}).then(res => res.json()).then(json => console.log(json));
